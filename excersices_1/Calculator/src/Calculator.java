56e6703876d2e4fc861dd0c24933ad9738145556
Maciej Cieplowski <maciejcieplowski@gmail.com>
2026-02-11 13:32:25 +0100

    +add Calculator exercise

---
+++ excersices_1/Calculator/src/Calculator.java
@@ -0,0 +1,33 @@
+import static java.lang.Math.pow;
+
+public class Calculator {
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double substract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by 0 is not allowed");
+        }
+        return a / b;
+    }
+
+    public double modulo(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division modulo by 0 is not allowed");
+        }
+        return a % b;
+    }
+
+    public double powerFirstNumber(double a, double b) {
+        return pow(a, b);
+    }
+}
---
+++ excersices_1/Calculator/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        char decision = 'y';
+        double lastResult = 0;
+
+        while (decision != 'n') {
+            System.out.print("Write first number: ");
+            double firstNum = scanner.nextDouble();
+
+            System.out.print("Write mathematical operator (available: +, -, *, /, %, ^): ");
+            char operator = scanner.next().charAt(0);
+
+            System.out.print("Write second number: ");
+            double secondNum = scanner.nextDouble();
+
+            double result = switch (operator) {
+                case '+' -> calculator.sum(firstNum, secondNum);
+                case '-' -> calculator.substract(firstNum, secondNum);
+                case '*' -> calculator.multiply(firstNum, secondNum);
+                case '/' -> calculator.division(firstNum, secondNum);
+                case '%' -> calculator.modulo(firstNum, secondNum);
+                case '^' -> calculator.powerFirstNumber(firstNum, secondNum);
+                default -> throw new IllegalArgumentException("Unknown operator: " + operator);
+            };
+
+            System.out.println("Result: " + result);
+            lastResult = result;
+
+            do {
+                System.out.print("Press n to quit or y to enter new calculation: ");
+                decision = scanner.next().charAt(0);
+            } while (decision != 'n' && decision != 'y');
+        }
+
+        System.out.println(lastResult > 0
+                ? "Last result grater then zero"
+                : "Last result is smaller or equals zero");
+
+        scanner.close();
+    }
+}
